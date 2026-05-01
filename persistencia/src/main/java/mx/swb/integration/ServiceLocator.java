28b2e084f0b537fa9095f1d6538daacf1dbdecaf
AyalaRazo <ayalarazojulio13@gmail.com>
2026-04-30 18:31:52 -0700

    Agregacion de DAOs

--- persistencia/src/main/java/mx/swb/integration/ServiceLocator.java
+++ persistencia/src/main/java/mx/swb/integration/ServiceLocator.java
@@ -1,14 +1,18 @@
 package mx.swb.integration;
 
 import jakarta.persistence.EntityManager;
-import mx.swb.DAO.ProductoDAO;
+import mx.swb.DAO.*;
 import mx.swb.persistence.HibernateUtil;
 
 public class ServiceLocator {
 
     private static ProductoDAO productoDAO;
+    private static LoteDAO loteDAO;
+    private static MovimientoInventarioDAO movimientoInventarioDAO;
+    private static VentaDAO ventaDAO;
+    private static CajaDAO cajaDAO;
 
-    private static EntityManager getEntityManager(){
+    public static EntityManager getEntityManager(){
         return HibernateUtil.getEntityManager();
     }
 
@@ -18,6 +22,42 @@ public class ServiceLocator {
             return productoDAO;
         } else{
             return productoDAO;
+        }
+    }
+
+    public static LoteDAO getInstanceLoteDAO(){
+        if (loteDAO == null) {
+            loteDAO = new LoteDAO(getEntityManager());
+            return loteDAO;
+        } else{
+            return loteDAO;
+        }
+    }
+
+    public static MovimientoInventarioDAO getInstanceMovimientoInventarioDAO(){
+        if (movimientoInventarioDAO == null) {
+            movimientoInventarioDAO = new MovimientoInventarioDAO(getEntityManager());
+            return movimientoInventarioDAO;
+        } else{
+            return movimientoInventarioDAO;
+        }
+    }
+
+    public static VentaDAO getInstanceVentaDAO(){
+        if (ventaDAO == null) {
+            ventaDAO = new VentaDAO(getEntityManager());
+            return ventaDAO;
+        } else{
+            return ventaDAO;
+        }
+    }
+
+    public static CajaDAO getInstanceCajaDAO(){
+        if (cajaDAO == null) {
+            cajaDAO = new CajaDAO(getEntityManager());
+            return cajaDAO;
+        } else{
+            return cajaDAO;
         }
     }
 }
