6f20ac18107eeb39bd2e35a301e1735dc3d2b23d
AyalaRazo <ayalarazojulio13@gmail.com>
2026-04-30 18:27:14 -0700

    Agregacion de DAOs

--- negocio/src/main/java/mx/swb/integration/ServiceFacadeLocator.java
+++ negocio/src/main/java/mx/swb/integration/ServiceFacadeLocator.java
@@ -1,15 +1,51 @@
 package mx.swb.integration;
 
-import mx.swb.facade.FacadeProducto;
+import jakarta.persistence.EntityManager;
+import mx.swb.DAO.*;
+import mx.swb.entity.Caja;
+import mx.swb.persistence.HibernateUtil;
 
 public class ServiceFacadeLocator {
 
-    private static FacadeProducto facadeProducto;
+    private static ProductoDAO productoDAO;
+    private static VentaDAO ventaDAO;
+    private static LoteDAO loteDAO;
+    private static MovimientoInventarioDAO movimientoInventarioDAO;
+    private static CajaDAO cajaDAO;
 
-    public static FacadeProducto getInstanceFacadeUsuario() {
-        if (facadeProducto == null) {
-            facadeProducto = new FacadeProducto();
-        }
-        return facadeProducto;
+
+    public static EntityManager getEntityManager() {
+        return HibernateUtil.getEntityManager();
+    }
+
+    public static ProductoDAO getInstanceProductoDAO() {
+        if (productoDAO == null)
+            productoDAO = new ProductoDAO(getEntityManager());
+        return productoDAO;
+    }
+
+    public static VentaDAO getInstanceVentaDAO() {
+        if (ventaDAO == null)
+            ventaDAO = new VentaDAO(getEntityManager());
+        return ventaDAO;
+    }
+
+    public static LoteDAO getInstanceLoteDAO() {
+        if (loteDAO == null)
+            loteDAO = new LoteDAO(getEntityManager());
+        return loteDAO;
+    }
+
+    public static MovimientoInventarioDAO getInstanceMovimientoInventarioDAO() {
+        if (movimientoInventarioDAO == null)
+            movimientoInventarioDAO = new MovimientoInventarioDAO(getEntityManager());
+        return movimientoInventarioDAO;
+    }
+
+    public static CajaDAO getInstanceCajaDAO() {
+        if (cajaDAO == null)
+            cajaDAO = new CajaDAO(getEntityManager());
+        return cajaDAO;
     }
 }
+
